fe7d07ba1e74b1acc2e2b26d53a0a7d8a8d2f7c1
Andres Dre Gonzalez <ag1548@txstate.edu>
2025-09-18 23:42:44 -0500

    Add examples 9, 9, 10, and 11

---
+++ examples/11_fp_math_and_precision/fp_precision.cpp
@@ -0,0 +1,34 @@
+/* 
+ * This program illustrates issues associated with floating-point representation
+ * 
+ * @author: Apan Qasem <apan@txstate.edu>
+ * @date: 02/08/22
+ * 
+ * 
+ * 1. Study the code. What is the expected output? 
+ * 2. Compile and run the code. Is the output what you expected? 
+ * 3. Can you identify the bug? (use your knowledge of FP representation from Lec 5)
+ * 4. Fix the bug. 
+ */
+
+#include<iostream>
+
+using namespace std; 
+
+const float LOOP_BOUND = 1e+08;               // hundred million 
+const unsigned long PRINT_INTERVAL = 1e+06;   // one million
+
+int main(int argc, char *argv[]) {
+
+  float i = 0; 
+  unsigned j = 0;
+  while (i  <= LOOP_BOUND) {
+    // print counter value every millionth iteration
+    if (j % PRINT_INTERVAL == 0)
+      cout << i << endl;
+    i++;
+    j++;
+  }
+
+  return 0;
+}
