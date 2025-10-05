3f5aabcf64a7f6e9ab7d6c5dc5c6f318db8bc5a2
Andres Dre Gonzalez <ag1548@txstate.edu>
2025-10-05 14:11:11 -0500

    Add comments to fp examples of binary values

--- examples/13_fp_math_and_precision/fp_math.cpp
+++ examples/13_fp_math_and_precision/fp_math.cpp
@@ -1,24 +1,24 @@
-/* 
+/*
  * This program illustrates issues associated with floating-point representation
- * 
+ *
  * @author: Apan Qasem <apan@txstate.edu>
  * @date: 02/08/22
- * 
- * 
- * 1. Study the code. What is the expected output? 
- * 2. Compile and run the code. Is the output what you expected? 
+ *
+ *
+ * 1. Study the code. What is the expected output?
+ * 2. Compile and run the code. Is the output what you expected?
  * 3. Can you explain why we are getting this output? (use your knowledge of FP representation from Lec 5)
  */
 
 #include<iostream>
 
-using namespace std; 
+using namespace std;
 
 int main() {
 
-  float x = -1e+14;
-  float y = 1e+14;
-  float z = 1.00; 
+  float x = -1e+14; // -1.01... * 2 ^ 46
+  float y = 1e+14;  // -1.01... * 2 ^ 46
+  float z = 1.00;
 
   cout << x + y + z << endl;
   cout << (x + y) + z << endl;
